ad022c054de9e6b545a1f1b52e6c7a5eb37ba2e2
Фёдор Соколов <fsokolof@gmail.com>
2024-03-19 16:55:46 +0100

    Hope

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -63,7 +63,7 @@ WSGI_APPLICATION = 'foodgram.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': os.getenv('DB_ENGINE', default='django.db.backends.postgresql'),
-        'NAME': os.getenv('POSTGRES_DB', default='django_fodgram'),
+        'NAME': os.getenv('POSTGRES_DB', default='django_foodgram'),
         'USER': os.getenv('POSTGRES_USER', default='postgres'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD', default=''),
         'HOST': os.getenv('DB_HOST', '127.0.0.1'),
