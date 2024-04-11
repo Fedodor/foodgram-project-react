9bbb32c7bf250391d8f66c4a93d4a9794211d227
Фёдор Соколов <fsokolof@gmail.com>
2024-04-11 18:23:07 +0200

    Final

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -14,7 +14,7 @@ class IngredientFilter(FilterSet):
 
 
 class RecipeFilter(FilterSet):
-    # tags = AllValuesMultipleFilter(field_name='tags__slug')
+    tags = AllValuesMultipleFilter(field_name='tags__slug__in')
     is_favorited = NumberFilter(
         method='filter_is_favorited'
     )
